5b35e3c0c061d69b2960a2eb73739fdc272357ff
Faneva <fanevarakotomalala77@gmail.com>
2023-04-17 19:26:22 +0200

    modif du 17-04-23

--- src/Components/Home.tsx
+++ src/Components/Home.tsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { getPublicContent } from "../services/user.service";
 import logotaximoto from '../assets/logotaximoto.png';
+import './style.css';
 const Home: React.FC = () => {
   const [content, setContent] = useState<string>("");
   useEffect(() => {
@@ -19,7 +20,12 @@ const Home: React.FC = () => {
   }, []);
   return (
     <div className="container">
-      <img src= {logotaximoto} alt="taximoto" className="logotaxi"  />
+      <div className=" mycard">
+        <img src= {logotaximoto} alt="taximoto" className="logotaxi"  />
+        <h1>Bienvenue!</h1>
+        <p className="lead">Votre sécurité est notre priorité , voyager sans crainte avec nous.  Nous pouvons vous enmener où vous voulez en rien de temps. Vous pouvez nous contacter n'importe où et n'importe quand.</p>
+      </div>
+      
     </div>
   );
 };
